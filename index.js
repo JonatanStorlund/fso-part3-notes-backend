8ad5852c6132bbab75aad514bf0ab16c4f66ed0d
Jonatan Storlund <storlundjonatan@gmail.com>
2022-04-09 11:44:42 +0300

    add static build to heroku

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const app = express()
 
 app.use(cors())
-app.use(express.json())
+app.use(express.static('build'))
 
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :response-time :body'))
@@ -32,10 +32,6 @@ let persons = [
     "number": "39-23-6423122"
   }
 ]
-
-app.get('/', (req, res) => {
-  res.send('<div id="root"></div>')
-})
 
 app.get('/info', (req, res) => {
 
